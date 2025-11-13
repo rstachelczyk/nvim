d8a84f0d2ffeab9910634284e5a56fe959f8dfdd
Ryan Stachelczyk <ryans@mitchells.com>
2025-11-13 16:05:47 -0500

    Update flutter config to allow debugging

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -1,8 +1,16 @@
 return {
-  -- "nvim-flutter/flutter-tools.nvim",
-  -- lazy = false,
-  -- dependencies = {
-  --   "nvim-lua/plenary.nvim",
-  -- },
-  -- config = true,
+  "nvim-flutter/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("flutter-tools").setup({
+      debugger = {
+        enabled = true,
+      },
+    })
+    vim.keymap.set("n", "<leader>fr", "<cmd>FlutterReload<CR>", {})
+    vim.keymap.set("n", "<leader>fR", "<cmd>FlutterRestart<CR>", {})
+  end,
 }
