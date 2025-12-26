f1b756764629341382adc027cc6abf623b6846c5
Ryan Stachelczyk <ryans@mitchells.com>
2025-12-26 15:01:03 -0500

    Add LSP Code Lens support and file navigation from controller to view in Rails

--- .config/nvim/lua/plugins/lsp-config-overrides.lua
+++ .config/nvim/lua/plugins/lsp-config-overrides.lua
@@ -16,6 +16,27 @@ return {
           -- cmd = { "mise", "x", "--", "ruby-lsp" },
         },
       },
+
+      codelens = {
+        enabled = true,
+      },
+      setup = {
+        ruby_lsp = function(_, _)
+          vim.lsp.commands["rubyLsp.openFile"] = function(cmd, _)
+            local uri_frag = cmd.arguments[1][1]
+            local uri, line = uri_frag:match("^(.+)#L(%d+)$")
+            if not uri then
+              uri = uri_frag
+            end
+            local bufnr = vim.uri_to_bufnr(uri)
+            vim.fn.bufload(bufnr)
+            vim.api.nvim_set_option_value("buflisted", true, { buf = bufnr })
+            vim.api.nvim_set_current_buf(bufnr)
+
+            vim.api.nvim_win_set_cursor(0, { tonumber(line) or 1, 0 })
+          end
+        end,
+      },
     },
   },
 }
